6683d5052c84f95c04d91b1add7ca0d170454bd9
Natsuki Ikeguchi <n_ikeguchi@yumemi.co.jp>
2023-07-26 13:12:21 +0900

    style: Use space indents

--- index.ts
+++ index.ts
@@ -4,33 +4,33 @@ import { debug, error, getInput, info, setOutput } from '@actions/core';
 import { Octokit } from '@octokit/rest';
 
 const getInputRequired = (name: string) =>
-	getInput(name, {
-		required: true,
-	});
+  getInput(name, {
+    required: true,
+  });
 
 (async () => {
-	const environment = getInputRequired('environment');
-	const repositoryId = getInputRequired('repository_id');
-	const token = getInputRequired('token');
+  const environment = getInputRequired('environment');
+  const repositoryId = getInputRequired('repository_id');
+  const token = getInputRequired('token');
 
-	const octokit = new Octokit({
-		auth: token,
-	});
+  const octokit = new Octokit({
+    auth: token,
+  });
 
-	const env = await octokit.actions.listEnvironmentVariables({
-		repository_id: parseInt(repositoryId),
-		environment_name: environment,
-	});
+  const env = await octokit.actions.listEnvironmentVariables({
+    repository_id: parseInt(repositoryId),
+    environment_name: environment,
+  });
 
-	env.data.variables.forEach((v) => {
-		setOutput(v.name, v.value);
-		info(`Successfully set ${v.name}=${v.value}`);
-	});
+  env.data.variables.forEach((v) => {
+    setOutput(v.name, v.value);
+    info(`Successfully set ${v.name}=${v.value}`);
+  });
 
-	debug('Done');
+  debug('Done');
 })()
-	.then()
-	.catch((e) => {
-		error(e);
-		exit(1);
-	});
+  .then()
+  .catch((e) => {
+    error(e);
+    exit(1);
+  });
